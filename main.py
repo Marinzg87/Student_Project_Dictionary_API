eedd4e61aedcf5824e127c6650466930627643ca
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-02-28 23:43:06 +0100

    Add comments.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 
+# Create an instance of Flask object
+# __name__ due to the fact, that I want run this script only when the file is run, \
+# not when the function is importer to another file
 app = Flask(__name__)
 
 
+# This will create a link to home page
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
+# This will create a link to the API page
 @app.route("/api/v1/<word>")
 def api(word):
     definition = word.upper()
@@ -15,5 +20,6 @@ def api(word):
     return result_dictionary
 
 
+# Run API on the specific port
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
